89e0cffd12ddf3c0d2f50dbc6bb411744c12f2c7
Luke Yelavich <themuso@ubuntu.com>
2009-08-04 11:46:25 +0100

    Import patches-unapplied version 1:0.9.16~test3-0ubuntu1 to ubuntu/karmic
    Imported using git-ubuntu import.
    
    Changelog parent: a2e8187c3178134f95ce44a9a7657340dabd53b2
    
    New changelog entries:
      * New upstream release
      * debian/pulseaudio.postinst:
        - Do not create the pulse-rt group any more
        - Do not make the pulseaudio binary suid root
      * debian/pulseaudio.postrm: Do not try to remove the pulse-rt group
      * debian/pulseaudio.preinst: Remove the pulse-rt group on upgrade
      * debian/control:
        - Suggest rtkit
        - Renamed pulseaudio-module-hal and pulseaudio-module-hal-dbg to
          pulseaudio-module-udev and pulseaudio-module-udev-dbg respectively
        - pulseaudio-module-udev replaces pulseaudio-module-hal
        - Bumped libudev-dev requirement to >= 143
        - Added build dependencies libICE-dev, libxi-dev, x11proto-core-dev
        - Add new package for the rygel-media-server module
        - Tighten libasound2-dev and libsndfile1-dev dependencies
      * debian/pulseaudio.init: Change all hal references to udev
      * debian/patches/0008-clean-out-old-files.patch: Remove non-existant files
        from POTFILES.in, patch sent upstream
      * Merge from Debian unstable, remaining changes:
        - epoch (my stupid fault :S)
        - Don't build against, and create jack package. Jack is not in main
        - use speex-float-1 resampler to work better with lack of PREEMPT in
          karmic's -generic kernel config, also change buffer size
        - Add alsa configuration files to route alsa applications via pulseaudio
        - Move libasound2-plugins from Recommends to Depends
        - Add pm-utils sleep hook to suspend (and resume) users' pulseaudio
          daemons
        - patch to fix source/sink and suspend-on-idle race
        - Make initscript more informative in the default case of per-user
          sessions
        - add status check for system wide pulseaudio instance
        - create /var/run/pulse, and make restart more robust
        - LSB {Required-*,Should-*} should specify hal instead of dbus,
          since hal is required (and already requires dbus)
        - indicate that the system pulseaudio instance is being started from the init
          script
        - Install more upstream man pages
        - Link to pacat for parec man page
        - check whether pulseaudio is running before preloading the padsp library
        - Add DEB_OPT_FLAG = -O3 as per recommendation from
          pulseaudio-discuss/2007-December/001017.html
        - cache /usr/share/sounds/ubuntu/stereo/ wav files on pulseaudio load
        - Generate a PO template on build
        - add special case to disable pulseaudio loading if accessibility/speech
          is being used
        - the esd wrapper script should not load pulseaudio if pulseaudio is being
          used as a system service
        - add a pulseaudio apport hook
        - fix some typos in README.Debian
        - drop padevchooser(Recommends) and pavucontrol (Suggests)
        - drop libasyncns-dev build dependency, its in universe
        - add libudev-dev as a build-dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@
 # along with PulseAudio; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 
-VERSION=1.10
+VERSION=1.11
 
 run_versioned() {
     local P
